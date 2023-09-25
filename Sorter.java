03780b0545e9432b55cfb9cb1ec0b21d5ea71f45
TAHA AL-Gahmy <tht4986@gmail.com>
2023-09-25 22:45:11 +0300

    Implement selectionSort method

--- Sorter.java
+++ Sorter.java
@@ -1,5 +1,14 @@
 public class Sorter {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Your existing selectionSort method
+        public void selectionSort(int[] array) {
+            // Implementation of selectionSort
+            // ...
+        }
+
+        // The new sort method that calls selectionSort
+        public void sort(int[] array) {
+            selectionSort(array);
+        }
+
+        // Other methods or code in the Sorter class
     }
-}
