21aedfcdc033aa13694fe12e19f6ac3d1517ecae
TAHA AL-Gahmy <tht4986@gmail.com>
2023-09-26 14:26:55 +0300

    Initial commit

--- Sorter.java
+++ Sorter.java
@@ -1,14 +1,24 @@
 public class Sorter {
-        // Your existing selectionSort method
-        public void selectionSort(int[] array) {
-            // Implementation of selectionSort
-            // ...
-        }
+    // ... other class members ...
 
-        // The new sort method that calls selectionSort
-        public void sort(int[] array) {
-            selectionSort(array);
-        }
+    // Method to perform selection sort on an array
+    public static void selectionSort(int[] arr) {
+        int n = arr.length;
 
-        // Other methods or code in the Sorter class
+        for (int i = 0; i < n - 1; i++) {
+            int minIndex = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIndex]) {
+                    minIndex = j;
+                }
+            }
+
+            // Swap the elements at i and minIndex
+            int temp = arr[i];
+            arr[i] = arr[minIndex];
+            arr[minIndex] = temp;
+        }
     }
+
+    // ... other class members ...
+}
