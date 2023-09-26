cf945ca3328dc4836c23f3dbdcf83b8d5ae6f987
TAHA AL-Gahmy <tht4986@gmail.com>
2023-09-26 21:52:10 +0300

    Implement sort method

--- Sorter.java
+++ Sorter.java
@@ -1,24 +1,49 @@
 public class Sorter {
-    // ... other class members ...
-
-    // Method to perform selection sort on an array
-    public static void selectionSort(int[] arr) {
-        int n = arr.length;
+    public static void selectionSort(int[] array) {
+        int n = array.length;
 
         for (int i = 0; i < n - 1; i++) {
             int minIndex = i;
+
             for (int j = i + 1; j < n; j++) {
-                if (arr[j] < arr[minIndex]) {
+                if (array[j] < array[minIndex]) {
                     minIndex = j;
                 }
             }
 
-            // Swap the elements at i and minIndex
-            int temp = arr[i];
-            arr[i] = arr[minIndex];
-            arr[minIndex] = temp;
+            // Swap the found minimum element with the element at index i
+            int temp = array[i];
+            array[i] = array[minIndex];
+            array[minIndex] = temp;
         }
     }
 
-    // ... other class members ...
+    public static void insertionSort(int[] array) {
+        int n = array.length;
+
+        for (int i = 1; i < n; i++) {
+            int key = array[i];
+            int j = i - 1;
+
+            // Move elements of array[0..i-1] that are greater than key
+            // to one position ahead of their current position
+            while (j >= 0 && array[j] > key) {
+                array[j + 1] = array[j];
+                j--;
+            }
+
+            array[j + 1] = key;
+        }
+    }
+
+    // Sort using insertion sort
+    public static void sortWithInsertionSort(int[] array) {
+        insertionSort(array);
+    }
+
+    // Sort using selection sort
+    public static void sortWithSelectionSort(int[] array) {
+        selectionSort(array);
+    }
+
 }
